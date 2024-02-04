0db9c19314a8c13836160458ee59fd794c19a597
robeat1 <dzienisz.robert@gmail.com>
2024-02-04 14:30:33 +0100

    Dodaj pliki projektów.

---
+++ rdzienisz/Data/rdzieniszContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using rdzienisz.Models;
+
+namespace rdzienisz.Data
+{
+    public class rdzieniszContext : DbContext
+    {
+        public rdzieniszContext (DbContextOptions<rdzieniszContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<rdzienisz.Models.Movie> Movie { get; set; } = default!;
+    }
+}
